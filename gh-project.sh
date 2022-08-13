2a0e48734fc76f74d89a8c5cb0e0f47e6248d177
snurer <suhedanureroglu@gmail.com>
2022-08-13 14:03:52 -0500

    added gh-project.sh

--- gh-project.sh
+++ gh-project.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Automation of creating a repository on GitHub and cloning the new repository to our machine.
+
+### Automation of creating a repository on GitHub and cloning the new repository to our machine. ###
+
+# Addig some colors to our bash script
 green="\033[1;3;32m"
 plain="\033[1;3m"
 red="\033[1;3;31;47m"
 end="\033[0m"
+
 # Check for the token environment variables
 function token_check() {
     if [[ -z "${TOKEN}" ]]; then
@@ -38,21 +42,21 @@ prerequisites
 
 # Create a new folder for your project
 echo "First, create a new folder to initialize your repository"
-read -p "Please type in your folder name:  " PROJECT_NAME
-mkdir $PROJECT_NAME
-cd $PROJECT_NAME
+    read -p "Please type in your folder name:  " PROJECT_NAME
+        mkdir $PROJECT_NAME
+        cd $PROJECT_NAME
 echo "Now you are in your new project folder!"
 
 # Name the new repository
 echo "Name for your new repository:"
 echo "Use single word or hyphen(-) between words."
-read REPO_NAME
+    read REPO_NAME
 
 echo "Enter a repo description: "
-read DESCRIPTION
+    read DESCRIPTION
 
 echo "What is your GitHub username?"
-read USERNAME
+    read USERNAME
 
 # Initialise the repo locally, create ablank README, and .gitignore file add and commit
 echo "Initializing your repo locally"
@@ -61,19 +65,19 @@ echo "Initializing your repo locally"
     touch .gitignore
     git add .
 echo "what is your commit message?" 
-read COMMIT_MESSAGE
-git commit -m "$COMMIT_MESSAGE"
+    read COMMIT_MESSAGE
+    git commit -m "$COMMIT_MESSAGE"
 
 # Use github API to create the repository
 echo "Creating your new repository!"
-clone_url=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token $TOKEN" https://api.github.com/user/repos -d '{"name":"'$REPO_NAME'","description":"'$DESCRIPTION'","homepage":"https://github.com","private":true}' | jq -r .clone_url)
-echo "This is your CLONE URL: $clone_url"
+    clone_url=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token $TOKEN" https://api.github.com/user/repos -d '{"name":"'$REPO_NAME'","description":"'$DESCRIPTION'","homepage":"https://github.com","private":true}' | jq -r .clone_url)
+        echo "This is your CLONE URL: $clone_url"
 
 # Add the remote GitHub repo to local repo and push
 echo "adding remote GitHub repo to local and push."
-git remote add origin $clone_url
-git branch -m master main
-git push -u origin main
+    git remote add origin $clone_url
+    git branch -m master main
+    git push -u origin main
 
 
 
