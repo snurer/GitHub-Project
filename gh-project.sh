898bfda3bd35502c246968824982113e4c7826da
snurer <suhedanureroglu@gmail.com>
2022-08-14 14:10:07 -0500

    updated gh-project.sh

--- gh-project.sh
+++ gh-project.sh
@@ -3,15 +3,17 @@
 ### Automation of creating a repository on GitHub and cloning the new repository to our machine. ###
 
 # Addig some colors to our bash script
-green="\033[1;3;32m"
-plain="\033[1;3m"
-red="\033[1;3;31;47m"
-end="\033[0m"
+green='\033[0;32m'
+magenta='\033[0;35m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+end='\033[0m'
+italic='\x1b[3m'
 
 # Check for the token environment variables
 function token_check() {
     if [[ -z "${TOKEN}" ]]; then
-    echo -e "Token environment variable is not set"
+    echo -e "$magenta Token environment variable is not set $end"
     read -p "Do you need instruction how to create token on GitHub (Y/n):" answer
 
         if [[ $answer == Y ]]; then 
@@ -20,9 +22,9 @@ function token_check() {
                 echo -e "Use this link to generate new token: https://github.com/settings/tokens"
             fi
         else
-            echo -e "Great then!"
+            echo -e "$green Great then! $end"
         fi
-    read -p "please type in your token:  " TOKEN
+    read -p "Please type in your token:  " TOKEN
     else
     TOKEN="$TOKEN"
     fi 
@@ -32,58 +34,51 @@ token_check
 # Installation of jq
 function prerequisites() {
     echo -e "Installing a prerequisite program 'jq'"
+        brew install jq
+        echo -e "$green Great! jq has been installed! $end"
+
         if [[ $? != 0 ]]; then
-            brew install jq
-        else
-            echo "install jq with your default package manager"
+            echo -e "$red Please install jq with your default package manager$end"
         fi
+    
 }
 prerequisites
 
 # Create a new folder for your project
-echo "First, create a new folder to initialize your repository"
+echo -e "$magenta First, create a new folder to initialize your repository$end"
     read -p "Please type in your folder name:  " PROJECT_NAME
         mkdir $PROJECT_NAME
         cd $PROJECT_NAME
-echo "Now you are in your new project folder!"
+echo "$yellow Now you are in your new project folder!$end"
 
 # Name the new repository
-echo "Name for your new repository:"
-echo "Use single word or hyphen(-) between words."
+echo -e "$magenta Name for your new repository:$end"
+echo -e "$italic (Use single word or hyphen(-) between words)"
     read REPO_NAME
 
-echo "Enter a repo description: "
+echo -e "$magenta Enter a repo description: $end"
     read DESCRIPTION
 
-echo "What is your GitHub username?"
+echo -e "$magenta What is your GitHub username? $end"
     read USERNAME
 
 # Initialise the repo locally, create ablank README, and .gitignore file add and commit
-echo "Initializing your repo locally"
+echo -e "$green Initializing your repo locally...$end"
     git init
     touch README.md
     touch .gitignore
     git add .
-echo "what is your commit message?" 
+echo -e "$magenta what is your commit message?$end" 
     read COMMIT_MESSAGE
     git commit -m "$COMMIT_MESSAGE"
 
 # Use github API to create the repository
-echo "Creating your new repository!"
+echo -e "$green Creating your new repository!$end"
     clone_url=$(curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token $TOKEN" https://api.github.com/user/repos -d '{"name":"'$REPO_NAME'","description":"'$DESCRIPTION'","homepage":"https://github.com","private":true}' | jq -r .clone_url)
-        echo "This is your CLONE URL: $clone_url"
+        echo -e "$green This is your CLONE URL $end: $clone_url"
 
 # Add the remote GitHub repo to local repo and push
-echo "adding remote GitHub repo to local and push."
+echo -e "$green adding remote GitHub repo to local and push. $end"
     git remote add origin $clone_url
     git branch -m master main
     git push -u origin main
-
-
-
-
-
-
-
-
-
